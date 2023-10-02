ead3b5154f040452f8a0b3281d4a997710a71c00
Fakiha-Mumtaz <fakiha.mumtaz12@gmail.com>
2023-10-02 23:08:07 +0500

    first commit

---
+++ src/app/heroes/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12,  firstName: 'Dr. Nice',lastName:'Mumtaz',city:'Karachi' },
+  { id: 13,  firstName: 'Bombasto' ,lastName:'tazeem',city:'Lahore'},
+  { id: 14, firstName: 'Celeritas' ,lastName:'Maham',city:'islamabad'},
+  { id: 15, firstName: 'Magneta' ,lastName:'Marry',city:'Karachi'},
+  { id: 16, firstName: 'RubberMan' ,lastName:'Mehak',city:'Karachi'},
+
+];
